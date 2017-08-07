alesaiko/Bake

commit f52caaf944d2d5f1376b83e21a46f9b0bf391688

Breakfast: Bake: Introduce v2.4.1
- Rewrite some parts of the script in better Bash
- Drop down some old and useless code
- Improve config_picker logic
- Cleanup the code. Script became ligher by 25%
- Switch to Google 4.9.x toolchain for all targets as they support it now

Change-Id: Ic54c223377f55b101ac928f73b0f45ea31994a32
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

core/source.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 reset
 
-#
-# Copyright (C) 2017, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017, Alex Saiko <solcmdr@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -12,12 +11,11 @@ reset
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
 
-bf_ver=2.4
+bf_ver=2.4.1
 
 delay() {
-	sleep 0.25
+	sleep 0.2
 }
 
 export txtbld=$(tput bold)
@@ -35,21 +33,53 @@ export bldblu=${txtbld}$(tput setaf 4)
 export bldmgt=${txtbld}$(tput setaf 5)
 export bldcya=${txtbld}$(tput setaf 6)
 
-export ROOTDIR=`readlink -f .`
-
+export ROOTDIR=$(readlink -f .)
 export BFCONFIGS="$ROOTDIR/core/configs"
 export OUTPUT="$ROOTDIR/output"
 
-export NR_CPUS=`grep 'processor' /proc/cpuinfo | wc -l`
-if [ $NR_CPUS -le "8" ]; then
-	NR_CPUS="8"
-fi
+export NR_CPUS=$(grep 'processor' /proc/cpuinfo | wc -l)
+[ $NR_CPUS -le "8" ] && NR_CPUS="8"
 
 update_cur_config() {
 	if [ -e $BFCONFIGS/.cur_config ]; then
 		export CUR_CONFIG=$(cat $BFCONFIGS/.cur_config 2>/dev/null)
 	fi
 }; update_cur_config
+
+config_picker() {
+	echo "${bldmgt}----- Available configs: ${txtrst}"; delay
+	ls $BFCONFIGS -1
+
+	if [ ! -z $CUR_CONFIG ]; then
+		echo "${bldmgt}----- Current config: ${txtrst}"; delay
+		echo $CUR_CONFIG
+	else
+		echo "${bldylw}----- No config selected!${txtrst}"; delay
+	fi
+
+	read -p "${bldmgt}----- Select the config: ${txtrst}" selected_config
+	if [ -z $selected_config ]; then
+		echo "${bldylw}----- No config selected!${txtrst}"; delay
+		exit 0
+	fi
+	export config_name=$(find $BFCONFIGS/$selected_config* -printf "%f\n" -quit)
+
+	if [ ! -z $config_name ]; then
+		echo "$config_name" > $BFCONFIGS/.cur_config
+		update_cur_config
+
+		if [ "$CUR_CONFIG" = "$config_name" ]; then
+			echo "${bldgrn}----- $config_name was successfully picked!${txtrst}"; delay
+		else
+			echo "${bldred}----- ERROR: $config_name was not picked!${txtrst}"; delay
+			exit 1
+		fi
+	else
+		echo "${bldred}----- ERROR: $config_name was not found!${txtrst}"; delay
+		echo "${bldred}----- Please, check out the config and try again!${txtrst}"; delay
+		exit 1
+	fi
+}
 
 load_defines() {
 	export KERNSOURCE="$ROOTDIR/src/kernel/$SOURCE"
@@ -59,253 +89,133 @@ load_defines() {
 	export VERSION="$SOURCE"
 }
 
-check() {
-	echo "${bldcya}----- Checking BF...${txtrst}"; delay
-
-	if [ ! -n $BFCONFIGS ]; then
-		echo "${bldred}----- ERROR: Configuration directory was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, ensure that you have installed Breakfast correctly!${txtrst}"; delay; exit 1
-	fi
-
-	if [ ! -n $KERNSOURCE ]; then
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
-	fi
-
-	if [ ! -f ${CROSS_COMPILE}gcc ]; then
-		echo "${bldred}----- ERROR: Toolchain was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download the toolchain!${txtrst}"; delay; exit 1
-	fi
-
-	if [ ! -f $KERNFLASHABLE/META-INF/com/google/android/update-binary ]; then
-		echo "${bldred}----- ERROR: No flashable structure found!${txtrst}"; delay
-		echo "${bldred}----- Please, establish the flashable structure first!${txtrst}"; delay; exit 1
-	fi
-}
-
-config_picker() {
-	if [ -n $BFCONFIGS ]; then
-		echo "${bldmgt}----- Available configs: ${txtrst}"; delay
-		ls $BFCONFIGS -1
-
-		update_cur_config
-		if [ -e $BFCONFIGS/.cur_config ] && [ $CUR_CONFIG ]; then
-			echo "${bldmgt}----- Current config: ${txtrst}"; delay
-			echo $CUR_CONFIG
-		else
-			echo "${bldylw}----- No config selected!${txtrst}"; delay
-		fi
-
-		read -p "${bldmgt}----- Select the config: ${txtrst}" selected_config
-		if [ ! $selected_config ]; then
-			echo "${bldylw}----- No config selected!${txtrst}"; delay; exit 0
-		fi
-		export config_name=$selected_config
-
-		if [ -e $BFCONFIGS/$selected_config ] ||
-		   [ -e $BFCONFIGS/${selected_config}.conf ]; then
-			if [ -e $BFCONFIGS/${selected_config}.conf ]; then
-				config_name="${selected_config}.conf"
-			fi
-
-			echo "$config_name" > $BFCONFIGS/.cur_config
-			update_cur_config
-
-			if [ "$CUR_CONFIG" = "$config_name" ]; then
-				echo "${bldgrn}----- $config_name was successfully picked!${txtrst}"; delay
-			else
-				echo "${bldred}----- ERROR: $config_name was not picked!${txtrst}"; delay exit 1
-			fi
-		else
-			echo "${bldred}----- ERROR: $config_name was not found!${txtrst}"; delay
-			echo "${bldred}----- Please, check out the config and try again!${txtrst}"; delay; exit 1
-		fi
-	else
-		echo "${bldred}----- ERROR: Configuration directory was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, ensure that you have installed Breakfast correctly!${txtrst}"; delay; exit 1
-	fi
-}
-
 config_loader() {
-	if [ -n $BFCONFIGS ]; then
-		update_cur_config
-		if [ ! $CUR_CONFIG ]; then
-			echo "${bldylw}----- WARNING: cur_config is empty!${txtrst}"; delay
-			echo "${bldylw}----- Initializing config_picker...${txtrst}"; delay
-			config_picker
-		fi
+	if [ -z $CUR_CONFIG ]; then
+		echo "${bldylw}----- WARNING: cur_config is empty!${txtrst}"; delay
+		echo "${bldylw}----- Calling config_picker...${txtrst}"; delay
+		config_picker
+	fi
 
-		source $BFCONFIGS/$CUR_CONFIG
-		load_defines
-
-		if [ ! $SOURCE ] || [ ! $ARCH ] ||
-		   [ ! $SUBARCH ] || [ ! $ORIGCONFIG ] ||
-		   [ ! $CONFIG ] || [ ! $TOOLCHAIN ] ||
-		   [ ! $IMGTYPE ] || [ ! $FLASHABLE ] ||
-		   [ ! $KERNSOURCE ] || [ ! $KERNFLASHABLE ] ||
-		   [ ! $CROSS_COMPILE ] || [ ! $KERNIMG ] ||
-		   [ ! $VERSION ]; then
-			echo "${bldred}----- ERROR: $CUR_CONFIG was not loaded!${txtrst}"; delay; exit 1
-		fi
-	else
-		echo "${bldred}----- ERROR: Configuration directory was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, ensure that you have installed Breakfast correctly!${txtrst}"; delay; exit 1
+	source $BFCONFIGS/$CUR_CONFIG && load_defines
+	if [ -z $SOURCE ] || [ -z $ARCH ] || [ -z $SUBARCH ] ||
+	   [ -z $ORIGCONFIG ] || [ -z $CONFIG ] || [ -z $TOOLCHAIN ] ||
+	   [ -z $IMGTYPE ] || [ -z $FLASHABLE ] || [ -z $KERNSOURCE ] ||
+	   [ -z $KERNFLASHABLE ] || [ -z $CROSS_COMPILE ] || [ -z $KERNIMG ] ||
+	   [ -z $VERSION ]; then
+		echo "${bldred}----- ERROR: $CUR_CONFIG was not loaded!${txtrst}"; delay
+		exit 1
 	fi
 }
 
-hard_clean() {
-	if [ -n $KERNSOURCE ]; then
-		rm -rf $KERNIMG				\
-		      $KERNFLASHABLE/kernel/$IMGTYPE
-	else
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
-	fi
-}
-
-clean_junk() {
-	if [ -n $KERNSOURCE ]; then
-		find $KERNSOURCE -type f \( -iname \*.rej	\
-			       -o -iname \*.orig		\
-			       -o -iname \*.bkp			\
-			       -o -iname \*.ko			\
-			       -o -iname \*.c.BACKUP.[0-9]*.c	\
-			       -o -iname \*.c.BASE.[0-9]*.c	\
-			       -o -iname \*.c.LOCAL.[0-9]*.c	\
-			       -o -iname \*.c.REMOTE.[0-9]*.c	\
-			       -o -iname \*.org \)		\
+clean_leftover() {
+	find $KERNSOURCE -type f \( -iname \*.rej		\
+				 -o -iname \*.orig		\
+				 -o -iname \*.bkp		\
+				 -o -iname \*.ko		\
+				 -o -iname \*.c.BACKUP.[0-9]*.c	\
+				 -o -iname \*.c.BASE.[0-9]*.c	\
+				 -o -iname \*.c.LOCAL.[0-9]*.c	\
+				 -o -iname \*.c.REMOTE.[0-9]*.c	\
+				 -o -iname \*.org \)		\
 					| parallel rm -fv {}
-	else
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
-	fi
+
+	rm -f $KERNIMG $KERNFLASHABLE/kernel/$IMGTYPE
 }
 
 clean() {
 	echo "${bldcya}----- Cleaning the source...${txtrst}"; delay
 
-	if [ -n $KERNSOURCE ]; then
-		if [ -e $KERNSOURCE/Makefile ]; then
-			cd $KERNSOURCE
+	if [ -e $KERNSOURCE/Makefile ]; then
+		cd $KERNSOURCE
+		make mrproper && make clean
+		cd $ROOTDIR
+	fi
 
-			make mrproper
-			make clean
+	clean_leftover
 
-			cd $ROOTDIR
-		fi
+	if [ "$ARCH" == "arm" ] && [ ! $CUR_CONFIG == "flo.conf" ]; then
+		rm -f $KERNSOURCE/arch/arm/mach-msm/smd_rpc_sym.c
+	fi
 
-		clean_junk
-		hard_clean
+	rm -rf $KERNSOURCE/arch/$ARCH/boot/*.dtb		\
+	       $KERNSOURCE/arch/$ARCH/boot/*.cmd		\
+	       $KERNSOURCE/arch/$ARCH/crypto/aesbs-core.S	\
+	       $KERNSOURCE/include/generated			\
+	       $KERNSOURCE/arch/*/include/generated
 
-		if [ "$ARCH" == "arm" ] && [ ! $CUR_CONFIG == "flo.conf" ]; then
-			rm -f $KERNSOURCE/arch/arm/mach-msm/smd_rpc_sym.c
-		fi
-
-		rm -rf $KERNSOURCE/arch/$ARCH/boot/*.dtb		\
-		       $KERNSOURCE/arch/$ARCH/boot/*.cmd		\
-		       $KERNSOURCE/arch/$ARCH/crypto/aesbs-core.S	\
-		       $KERNSOURCE/include/generated			\
-		       $KERNSOURCE/arch/*/include/generated
-
-		if [ -e $KERNSOURCE/scripts/basic/fixdep ]; then
-			echo "${bldred}----- ERROR: Tree was not cleaned!${txtrst}"; delay; exit 1
-		fi
-	else
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
+	if [ -e $KERNSOURCE/scripts/basic/fixdep ]; then
+		echo "${bldred}----- ERROR: Tree was not cleaned!${txtrst}"; delay
+		exit 1
 	fi
 }
 
-full_clean() {
-	if [ -n $KERNSOURCE ]; then
-		clean_junk
-		hard_clean
+intelli_clean() {
+	clean_leftover
 
-		if [ -e $KERNSOURCE/scripts/basic/fixdep ] || [ "$1" == "force" ]; then
-			if [ -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG ] || [ "$1" == "force" ]; then
-				clean
-			else
-				echo "${bldylw}----- WARNING: Defconfig hasn't been created yet!${txtrst}"; delay
-				echo "${bldylw}----- Skipping the cleaning...${txtrst}"; delay
-			fi
-		fi
-	else
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
+	if [ -e $KERNSOURCE/scripts/basic/fixdep ] || [ "$1" == "force" ]; then
+		clean
 	fi
 }
 
 crt_config() {
 	echo "${bldcya}----- Checking for defconfig...${txtrst}"; delay
 
-	if [ -n $KERNSOURCE ]; then
-		if [ ! -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG ]; then
-			echo "${bldcya}----- Creating defconfig...${txtrst}"; delay
+	if [ ! -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG ]; then
+		echo "${bldcya}----- Creating defconfig...${txtrst}"; delay
 
-			if [ ! -e $KERNSOURCE/arch/$ARCH/configs/$ORIGCONFIG ]; then
-				echo "${bldred}----- ERROR: $ORIGCONFIG was not found!${txtrst}"; delay; exit 1
-			fi
-
-			if [ "$CONFIG" == "$ORIGCONFIG" ]; then
-				echo "${bldred}----- ERROR: Configurations' names are same!${txtrst}"; delay; exit 1
-			fi
-
-			cd $KERNSOURCE
-
-			make $ORIGCONFIG
-			mv $KERNSOURCE/.config $KERNSOURCE/arch/$ARCH/configs/$CONFIG
-
-			cd $ROOTDIR
-
-			clean
-
-			if [ ! -e $KERNSOURCE/arch/$ARCH/configs/$CONFIG ]; then
-				echo "${bldred}----- ERROR: Defconfig was not created!${txtrst}"; delay; exit 1
-			fi
+		if [ ! -e $KERNSOURCE/arch/$ARCH/configs/$ORIGCONFIG ]; then
+			echo "${bldred}----- ERROR: $ORIGCONFIG was not found!${txtrst}"; delay
+			exit 1
 		fi
-	else
-		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
+
+		if [ "$CONFIG" == "$ORIGCONFIG" ]; then
+			echo "${bldred}----- ERROR: Configurations' names are same!${txtrst}"; delay
+			exit 1
+		fi
+
+		cd $KERNSOURCE && make $ORIGCONFIG
+
+		if [ -e $KERNSOURCE/.config ]; then
+			mv $KERNSOURCE/.config $KERNSOURCE/arch/$ARCH/configs/$CONFIG
+		else
+			echo "${bldred}----- ERROR: Generated .config disappeared!${txtrst}"; delay
+			exit 1
+		fi
+
+		cd $ROOTDIR && clean
 	fi
 }
 
 crt_flashable() {
 	echo "${bldcya}----- Creating flashable archive...${txtrst}"; delay
 
-	if [ -e $KERNFLASHABLE ]; then
-		cd $KERNFLASHABLE
+	cd $KERNFLASHABLE
 
-		rm -rf *.zip
+	rm -f *.zip
 
-		zip -r ${VERSION}-$(date +"%Y%m%d").zip .
+	export DATE=$(date +"%Y%m%d")
+	zip -r ${VERSION}-$DATE.zip .
 
-		if [ ! -e $OUTPUT/$SOURCE/ ]; then
-			mkdir -p $OUTPUT/$SOURCE/
+	[ ! -e $OUTPUT/$SOURCE/ ] && mkdir -p $OUTPUT/$SOURCE/
+	[ ! -e $OUTPUT/archived/$SOURCE/ ] && mkdir -p $OUTPUT/archived/$SOURCE/
+
+	if [ -e $KERNFLASHABLE/${VERSION}-$DATE.zip ]; then
+		if [ -e $OUTPUT/$SOURCE/${VERSION}-$DATE.zip ]; then
+			mv -f $OUTPUT/$SOURCE/${VERSION}-$DATE.zip $OUTPUT/archived/$SOURCE
 		fi
 
-		if [ ! -e $OUTPUT/archived/$SOURCE/ ]; then
-			mkdir -p $OUTPUT/archived/$SOURCE/
+		mv $KERNFLASHABLE/${VERSION}-$DATE.zip $OUTPUT/$SOURCE/
+
+		if [ ! -e $OUTPUT/$SOURCE/${VERSION}-$DATE.zip ]; then
+			echo "${bldred}----- ERROR: Flashable archive was not created!${txtrst}"; delay
+			exit 1
 		fi
-
-		if [ -e $KERNFLASHABLE/${VERSION}*.zip ]; then
-			if [ -e $OUTPUT/$SOURCE/${VERSION}*.zip ]; then
-				mv -f $OUTPUT/$SOURCE/${VERSION}*.zip $OUTPUT/archived/$SOURCE
-			fi
-
-			mv $KERNFLASHABLE/${VERSION}*.zip $OUTPUT/$SOURCE/
-
-			if [ ! -e $OUTPUT/$SOURCE/${VERSION}*.zip ]; then
-				echo "${bldred}----- ERROR: Flashable archive was not created!${txtrst}"; delay; exit 1
-			fi
-		else
-			echo "${bldred}----- ERROR: Flashable archive was not created!${txtrst}"; delay; exit 1
-		fi
-
-		cd $KERNSOURCE
 	else
-		echo "${bldred}----- ERROR: No flashable structure found!${txtrst}"; delay
-		echo "${bldred}----- Please, establish the flashable structure first!${txtrst}"; delay; exit 1
+		echo "${bldred}----- ERROR: Flashable archive was not created!${txtrst}"; delay
+		exit 1
 	fi
+
+	cd $ROOTDIR
 }
 
 crt_kernel() {
@@ -316,22 +226,22 @@ crt_kernel() {
 
 		cd $KERNSOURCE
 
-		make $CONFIG
-		make -j$NR_CPUS $IMGTYPE
+		make $CONFIG && make -j$NR_CPUS $IMGTYPE
 
 		if [ -e $KERNIMG ]; then
 			mv $KERNIMG $KERNFLASHABLE/kernel/
 
-			crt_flashable
-			clean
+			crt_flashable && clean
 
-			if [ -e $OUTPUT/$SOURCE/${VERSION}*.zip ]; then
+			if [ -e $OUTPUT/$SOURCE/$VERSION-$DATE.zip ]; then
 				echo "${bldmgt}----- Kernel was successfully built!${txtrst}"; delay
 			else
-				echo "${bldred}----- ERROR: Could not find the built kernel!${txtrst}"; delay; exit 0
+				echo "${bldred}----- ERROR: Could not find the built kernel!${txtrst}"; delay
+				exit 1
 			fi
 		else
-			echo "${bldred}----- ERROR: Kernel STUCK in build!${txtrst}"; delay; exit 1
+			echo "${bldred}----- ERROR: Kernel STUCK in build!${txtrst}"; delay
+			exit 1
 		fi
 
 		time2=$(date +"%s.%N")
@@ -340,12 +250,12 @@ crt_kernel() {
 		echo "${bldcya}----- Elapsed time: $elapsed_time seconds${txtrst}"; delay
 	else
 		echo "${bldred}----- ERROR: Linux source was not found!${txtrst}"; delay
-		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay; exit 1
+		echo "${bldred}----- Please, download Linux source!${txtrst}"; delay
+		exit 1
 	fi
 }
 
 init() {
-	check
 	crt_config
 
 	echo "${bldblu}----- Build starts in 3${txtrst}"; sleep 1

bake.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#
-# Copyright (C) 2017, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017, Alex Saiko <solcmdr@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -11,36 +10,33 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
 
-if [ -e core/source.sh ]; then
-	source core/source.sh
+# Specify breakfast core script directory
+BFCORE="core/source.sh"
+if [ -z $BFCORE ]; then
+	echo "----- ERROR: Source file was not found!"
+	echo "----- Please, ensure that you have $BFCORE script!"
+	exit 1
 else
-	echo "$(tput bold)$(tput setaf 1)ERROR: SOURCE FILE WAS NOT FOUND!$(tput sgr0)"; exit 1
+	source $BFCORE
 fi
 
-echo "${bldcya}----- Starting Breakfast v$bf_ver...${txtrst}"; delay;
-
-if [ ! "$1" == "--new" ]; then
-	config_loader
-fi
+echo "${bldcya}----- Starting Breakfast v$bf_ver...${txtrst}"; delay
+[ ! "$1" == "--new" ] && config_loader
 
 while [ $1 ]; do
 	if [ "$1" == "--new" ]; then
-		config_picker
-		config_loader
+		config_picker && config_loader
 	elif [ "$1" == "--config" ]; then
 		rm -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG
 		crt_config
 	elif [ "$1" == "--fresh" ]; then
 		rm -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG
-		crt_config
-		init
+		crt_config && init
 	elif [ "$1" == "--clean" ]; then
-		full_clean "force"
+		intelli_clean "force"
 	elif [ "$1" == "--destroy" ]; then
-		rm -rf $OUTPUT/archived/*.zip \
-		       $OUTPUT/*.zip
+		rm -rf $OUTPUT/*
 	else
 		echo "${bldred}----- ERROR: Unknown key. Continuing...${txtrst}"; delay
 		break
@@ -49,7 +45,4 @@ while [ $1 ]; do
 	exit 0
 done
 
-full_clean
-init
-
-exit 0
+intelli_clean && init

core/configs/flo.conf
@@ -7,6 +7,6 @@ export ARCH="arm"
 export SUBARCH="arm"
 export ORIGCONFIG="flo_defconfig"
 export CONFIG="breakfast_defconfig"
-export TOOLCHAIN="arm-eabi"
+export TOOLCHAIN="arm-linux-androideabi"
 export IMGTYPE="zImage"
 export FLASHABLE="flashable-flo"

core/configs/hammerhead.conf
@@ -7,6 +7,6 @@ export ARCH="arm"
 export SUBARCH="arm"
 export ORIGCONFIG="hammerhead_defconfig"
 export CONFIG="breakfast_defconfig"
-export TOOLCHAIN="arm-eabi"
+export TOOLCHAIN="arm-linux-androideabi"
 export IMGTYPE="zImage-dtb"
 export FLASHABLE="flashable-hammerhead"

commit 09729c194147510134f25a71ed53664125cab6ce

Breakfast: Bake: Fixup output files archivation
output/archived was broken since f52caaf944d2 because I changed
checking logic.

Check for every * archive in output directory.
Only one file should be there.

Change-Id: Ic98f606d86038e2acf3e3f44e1f4a8dec910e35b
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

core/source.sh
@@ -200,8 +200,8 @@ crt_flashable() {
 	[ ! -e $OUTPUT/archived/$SOURCE/ ] && mkdir -p $OUTPUT/archived/$SOURCE/
 
 	if [ -e $KERNFLASHABLE/${VERSION}-$DATE.zip ]; then
-		if [ -e $OUTPUT/$SOURCE/${VERSION}-$DATE.zip ]; then
-			mv -f $OUTPUT/$SOURCE/${VERSION}-$DATE.zip $OUTPUT/archived/$SOURCE
+		if [ -e $OUTPUT/$SOURCE/$VERSION*.zip ]; then
+			mv -f $OUTPUT/$SOURCE/$VERSION*.zip $OUTPUT/archived/$SOURCE
 		fi
 
 		mv $KERNFLASHABLE/${VERSION}-$DATE.zip $OUTPUT/$SOURCE/