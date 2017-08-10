b9b1f75c82dbf48225012f6f7a5daec94cb14ede
Alex Saiko <solcmdr@gmail.com>
2017-08-10 18:11:25 +0300

    Breakfast: Bake: Introduce v3.0.0
    This is a huge rewrite of the whole bake script.
    
    First of all, I got rid of source file, which was pretty useless
    considering the fact that nothing except bake.sh itself was using
    it. All the functions were written from scratch (except cleaning
    one) in a better language.
    
    Bake's functionality is significantly reduces, but its main feature
    (multitarget support) is here. You should run bake via
     "$ ./bake.sh <configuration_name>" or "$ sh bake.sh <configuration_name>"
    where "<configuration_name" is a full or a part name of the Bake
    configuration file. An input configuration file will be hooked by
    find_config function, which uses "find" command to determine the
    selected config's full name. Because of this, you can use shortcuts
    like "h" instead of "hammerhead.conf". Note that find command
    is used with a flag "-quit" meaning it will return the path (name
    in our case) for the first match only. If you have multiple configs
    with the same start (like "hammerhead.conf" and "hammerheadcaf.conf")
    the config which is lower in alphabetical order will be hooked.
    
    Configuration files itself are much simpler now because useless
    variables were removed and printing was moved to the main script.
    
    The only function of the script is building and packaging the
    kernel for a specific target. More functions (and previous
    functionality) will become later.
    
    Change-Id: I2003f04b313d10bbb7ed6562fbc0654a2125f2ad
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- bake.sh
+++ bake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 
 # Copyright (C) 2017, Alex Saiko <solcmdr@gmail.com>
 #
@@ -11,38 +12,173 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-# Specify breakfast core script directory
-BFCORE="core/source.sh"
-if [ -z $BFCORE ]; then
-	echo "----- ERROR: Source file was not found!"
-	echo "----- Please, ensure that you have $BFCORE script!"
-	exit 1
-else
-	source $BFCORE
-fi
+bf_ver=3.0.0
 
-echo "${bldcya}----- Starting Breakfast v$bf_ver...${txtrst}"; delay
-[ ! "$1" == "--new" ] && config_loader
+clr_red=$(tput bold)$(tput setaf 1);
+clr_green=$(tput bold)$(tput setaf 2);
+clr_yellow=$(tput bold)$(tput setaf 3);
+clr_blue=$(tput bold)$(tput setaf 4);
+clr_magenta=$(tput bold)$(tput setaf 5);
+clr_cyan=$(tput bold)$(tput setaf 6);
+clr_reset=$(tput sgr0);
 
-while [ $1 ]; do
-	if [ "$1" == "--new" ]; then
-		config_picker && config_loader
-	elif [ "$1" == "--config" ]; then
-		rm -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG
-		crt_config
-	elif [ "$1" == "--fresh" ]; then
-		rm -f $KERNSOURCE/arch/$ARCH/configs/$CONFIG
-		crt_config && init
-	elif [ "$1" == "--clean" ]; then
-		intelli_clean "force"
-	elif [ "$1" == "--destroy" ]; then
-		rm -rf $OUTPUT/*
-	else
-		echo "${bldred}----- ERROR: Unknown key. Continuing...${txtrst}"; delay
-		break
+root_dir=$(readlink -f .);
+configs_dir="${root_dir}/configs"
+kernels_dir="${root_dir}/kernels"
+flashables_dir="${root_dir}/flashables"
+toolchains_dir="${root_dir}/toolchains"
+outputs_dir="${root_dir}/outputs"
+
+let "nr_cpus = $(grep "processor" /proc/cpuinfo | wc -l) * 2"
+
+print()
+{
+	# Delay is required to make output smoother
+	printf "$1\n"; sleep 0.125
+}
+
+terminate()
+{
+	[ "$1" == "-1" ] && error="Bake is damaged!\n----- Broken part -> $2"
+	[ "$1" == "0" ] && error="No argument passed!"
+	[ "$1" == "1" ] && error="$2 was not found!"
+	[ "$1" == "2" ] && error="$2 was not loaded!"
+	[ "$1" == "3" ] && error="Kernel stuck in build!"
+	[ "$1" == "4" ] && error="Could not create flashable archive!"
+
+	print "${clr_red}----- ERROR: $error\n\nScript terminated with error $1${clr_reset}"
+	exit $1
+}
+
+check_bake_init()
+{
+	print "${clr_cyan}----- Starting Bake v${bf_ver}...${clr_reset}"
+
+	for dir in $configs_dir $kernels_dir $flashables_dir \
+		   $toolchains_dir $outputs_dir; do
+		[ -s $dir ] || terminate "-1" "$dir"
+	done
+}; check_bake_init
+
+find_config()
+{
+	export cur_config=$(find $configs_dir/$1* -printf "%f\n" -quit 2>/dev/null);
+	[ $cur_config ] || terminate "1" "$1"
+}
+
+check_args()
+{
+	[ "$1" == "config" ] &&
+	if [ ! $ARCH ] || [ ! $SUBARCH ] || [ ! $flashable_name ] ||
+	   [ ! $kernel_name ] || [ ! $kernel_image ] || [ ! $defconfig ] ||
+	   [ ! $toolchain ]; then
+		return 0
 	fi
 
-	exit 0
-done
+	return 1
+}
 
-intelli_clean && init
+init_bake_config()
+{
+	print "${clr_magenta}----- Loading $1...${clr_reset}"
+
+	source ${configs_dir}/$1
+	if (check_args "config"); then terminate "2" "$1"; fi
+
+	export CROSS_COMPILE="$toolchains_dir/$toolchain/bin/${toolchain}-"
+
+	print "${clr_green}----- $1 was successfully loaded!${clr_reset}"
+}
+
+cleanup_kernel_tree()
+{
+	if [ -d $kernels_dir/$kernel_name ]; then
+		print "${clr_cyan}----- Cleaning ${kernel_name}...${clr_reset}"
+
+		cd $kernels_dir/$kernel_name
+
+		[ -f scripts/basic/fixdep ] && (make clean; make mrproper);
+
+		rm -rf arch/$ARCH/boot/*.dtb		\
+		       arch/$ARCH/boot/*.cmd		\
+		       arch/$ARCH/crypto/aesbs-core.S	\
+		       arch/*/include/generated		\
+		       include/generated
+
+		find . -type f \( -iname \*.rej			\
+			       -o -iname \*.orig		\
+			       -o -iname \*.bkp			\
+			       -o -iname \*.ko			\
+			       -o -iname \*.c.BACKUP.[0-9]*.c	\
+			       -o -iname \*.c.BASE.[0-9]*.c	\
+			       -o -iname \*.c.LOCAL.[0-9]*.c	\
+			       -o -iname \*.c.REMOTE.[0-9]*.c	\
+			       -o -iname \*.org \)		\
+					| parallel rm -fv {}
+
+		cd $root_dir
+	else
+		terminate "-1" "$kernels_dir/$kernel_name"
+	fi
+}
+
+make_flashable()
+{
+	print "${clr_cyan}----- Creating flashable archive...${clr_reset}"
+
+	cd $flashables_dir
+
+	[ -d $kernel_name ] || mkdir -p $kernel_name/kernel
+
+	rm -f $flashables_dir/$kernel_name/kernel/$kernel_image
+	mv $kernels_dir/$kernel_name/arch/$ARCH/boot/$kernel_image $kernel_name/kernel
+
+	date=$(date +"%Y%m%d");
+	cd $kernel_name && zip -r ${kernel_name}-$date.zip . && cd $flashables_dir
+
+	[ -d $outputs_dir/$kernel_name/archived ] ||
+	mkdir -p $outputs_dir/$kernel_name/archived
+	[ -f $outputs_dir/$kernel_name/$kernel_name* ] &&
+	mv $outputs_dir/$kernel_name/$kernel_name* $outputs_dir/$kernel_name/archived/
+
+	mv $flashables_dir/$kernel_name/${kernel_name}-$date.zip $outputs_dir/$kernel_name/
+	[ -f $outputs_dir/$kernel_name/${kernel_name}-$date.zip ] || terminate "4"
+
+	cd $rootdir
+}
+
+make_kernel()
+{
+	if [ -d $kernels_dir/$kernel_name ]; then
+		print "${clr_cyan}----- Building ${kernel_name}...${clr_reset}"
+		print "${clr_blue}----- Build starts in 3${clr_reset}"; sleep 1
+		print "${clr_blue}----- Build starts in 2${clr_reset}"; sleep 1
+		print "${clr_blue}----- Build starts in 1${clr_reset}"; sleep 1
+
+		start_time=$(date +"%s.%N");
+
+		cd $kernels_dir/$kernel_name
+
+		make $defconfig && make -j$nr_cpus $kernel_image
+
+		[ -f arch/$ARCH/boot/$kernel_image ] &&
+		(make_flashable; cleanup_kernel_tree) || terminate "3"
+
+		cd $root_dir
+
+		end_time=$(date +"%s.%N");
+		elapsed_time=$(echo "scale=1; ($end_time - $start_time) / 1" | bc);
+
+		print "${clr_magenta}----- Kernel was successfully built!${clr_reset}"
+		print "${clr_cyan}----- Elapsed time: $elapsed_time seconds${clr_reset}"
+	else
+		terminate "-1" "$kernels_dir/$kernel_name"
+	fi
+}
+
+[ $1 ] || terminate "0"
+
+find_config $1
+init_bake_config $cur_config
+cleanup_kernel_tree
+make_kernel
