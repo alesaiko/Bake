a1d2885d4fe21d5b059290fe7ed02341b53bcb2e
Alex Saiko <solcmdr@gmail.com>
2018-02-13 22:21:11 +0300

    Breakfast: Bake: Introduce v3.2
    This is a cleanup of unused code with some miscellaneous adjustements and
    freshly added useless commentaries. Also I've reduced the duplication of
    flashable installer code by unifying it between all supported targets. Now
    the installer code is completely device-independent (it was to be so
    previously too, by the way) and is placed in a separate directory called
    breakfast-installer.  Device-dependent scripts and resources will be
    squashed with installer during the process of flashable archive creation,
    hence the functionality is untouched.
    
    Change-Id: Ifab123f9e0fef5b4044939c2039339695b37645a
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- bake.sh
+++ bake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-# Copyright (C) 2017, Alex Saiko <solcmdr@gmail.com>
+# Copyright (C) 2017-2018, Alex Saiko <solcmdr@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -12,29 +12,41 @@ clear
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-BFVER=3.1.1
+BFVER=3.2
 
+# Initial terminal color plan.
 REDCL=$(tput bold)$(tput setaf 1)
 GRNCL=$(tput bold)$(tput setaf 2)
 BLUCL=$(tput bold)$(tput setaf 4)
 MGTCL=$(tput bold)$(tput setaf 5)
 CYACL=$(tput bold)$(tput setaf 6)
+# A flag to reset all applied colors.
 RSTCL=$(tput sgr0)
 
+# Current directory represents the directory where this script has been called.
+# Root directory represents the directory where this script is located.
 CRDIR=$(pwd)
 RTDIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-CFDIR="${RTDIR}/configs"
-KNDIR="${RTDIR}/kernels"
-SADIR="${RTDIR}/signapk"
-FSDIR="${RTDIR}/flashables"
-TCDIR="${RTDIR}/toolchains"
-OPDIR="${RTDIR}/outputs"
+CFDIR="$RTDIR/configs"
+KNDIR="$RTDIR/kernels"
+SADIR="$RTDIR/signapk"
+FSDIR="$RTDIR/flashables"
+FSTMP="$FSDIR/.tmp"
+BINST="$FSDIR/breakfast-installer"
+TCDIR="$RTDIR/toolchains"
+OPDIR="$RTDIR/outputs"
+
+# Bake uses the number of threads which is equal to the doubled
+# num of virtual processors to boost the compilation.
 NRCPS=$[ $(grep "processor" /proc/cpuinfo | wc -l) * 2 ]
 
+# Automatical Git tagging support.
 AUTAG="true"
 TAGNM="breakfast"
 
-print() { printf "$1${RSTCL}\n"; sleep 0.125; }
+# A simple color-expecting print function with a 0.125 delay for smooth output
+# of concurrent prints.
+print() { printf "${1}$RSTCL\n"; sleep 0.125; }
 
 terminate()
 {
@@ -49,20 +61,26 @@ terminate()
 	*) error="Unknown error!";;
 	esac
 
-	print "${REDCL}- ERROR: $error\n\nScript terminated with error $1!" &&
-	sleep 2.875
+	# Remove temporary flashable directory as it will be overwritten anyway.
+	rm -rf $FSTMP
 
+	print "${REDCL}- ERROR: $error\n\nScript terminated with error $1!" &&
+	# Print uses a 0.125 seconds delay, hence 2.875 should be used here to
+	# provide overall 3 seconds delay before the exit.
+	sleep 2.875
 	exit $1
 }
 
 prepare_environment()
 {
 	print "${CYACL}- Preparing working environment..."
+	# Unset all the required variables to avoid the conflict.
 	for var in ARCH SUBARCH KNAME KTYPE DFCFG TOOLC; do unset $var; done
 }
 
 config_loaded()
 {
+	# Check whether all the required variables are actually set.
 	[ ! -z $ARCH ] && [ ! -z $SUBARCH ] && [ ! -z $KNAME ] &&
 	[ ! -z $KTYPE ] && [ ! -z $DFCFG ] && [ ! -z $TOOLC ] &&
 	[ ! -z $CROSS_COMPILE ] || return 1
@@ -72,17 +90,23 @@ config_loaded()
 
 load_config()
 {
+	# This function expects an argument to be passed.
 	[ ! -z $1 ] || terminate "1"
 
+	# Try to find a requested configuration in an appropriate directory.
 	CRCFG=$(find $CFDIR/$1* -printf "%f\n" -quit 2>/dev/null)
 	[ ! -z $CRCFG ] || terminate "2" "$1"
 
 	print "${MGTCL}- Loading new Bake configuration --> $CRCFG..."
+	# Prepare bash environment before sourcing the configuration file.
 	prepare_environment
 
+	# Load information from configuration file to bash environment and
+	# setup cross compiler path.
 	source ${CFDIR}/$CRCFG &&
 	export CROSS_COMPILE="$TCDIR/$TOOLC/bin/${TOOLC}-"
 
+	# Ensure all the things have been made properly.
 	print "${MGTCL}- Validating Bake configuration..."
 	(config_loaded) || terminate "3" "$CRCFG"
 
@@ -91,21 +115,26 @@ load_config()
 
 prepare_kernel_tree()
 {
+	# This function cannot work without a configuration loaded.
 	(config_loaded) || terminate "3" "Bake configuration"
 
+	# Ensure there is a work-ready kernel tree.
 	[ -f $KNDIR/$KNAME/Makefile ] && cd $KNDIR/$KNAME ||
 	terminate "2" "$KNDIR/$KNAME/Makefile"
 
 	print "${CYACL}- Preparing ${KNAME} tree..."
 
-	[ -f scripts/basic/fixdep ] && (make clean; make mrproper)
+	# Do basic clean-up of the tree via Makefile-provided methods.
+	[ -f scripts/basic/fixdep ] && (make clean mrproper)
 
+	# Remove leftovers of the compilation.
 	rm -rf arch/$ARCH/boot/*.dtb		\
 	       arch/$ARCH/boot/*.cmd		\
 	       arch/$ARCH/crypto/aesbs-core.S	\
 	       arch/*/include/generated		\
 	       include/generated
 
+	# Try to remove *unneeded* files via parallel if supported.
 	which parallel &>/dev/null
 	[ "$?" -eq "0" ] &&
 	find . -type f \( -iname \*.rej			\
@@ -119,137 +148,148 @@ prepare_kernel_tree()
 		       -o -iname \*.org \)		\
 				| parallel rm -fv { }
 
-	[ "$CRCFG" == "hammerhead.conf" ] &&
-	rm -f arch/$ARCH/mach-msm/smd_rpc_sym.c
+	# Tree-specific clean-ups.
+	case "$CRCFG" in
+	"hammerhead.conf") rm -f arch/$ARCH/mach-msm/smd_rpc_sym.c;;
+	esac
 
 	cd $CRDIR
 }
 
 sign_flashable()
 {
+	# This function expects both a configuration to be loaded and
+	# an argument to be passed.
 	(config_loaded) || terminate "3" "Bake configuration"
 	[ ! -z $1 ] || terminate "1"
 
+	# Check the presence of the required files.
 	[ -f $SADIR/signapk.jar ] && [ -f $SADIR/keys/*.pk8 ] &&
-	[ -f $SADIR/keys/*.pem ] && [ -f $FSDIR/$KNAME/$1 ] || return 1
+	[ -f $SADIR/keys/*.pem ] && [ -f $FSTMP/$1 ] || return 1
 
 	print "${CYACL}- Signing $1 archive..."
 
+	# Setup private and public keys.
 	PVKEY=$(find $SADIR/keys/*.pk8 | head -1)
 	PBKEY=$(find $SADIR/keys/*.pem | head -1)
 
-	java -jar $SADIR/signapk.jar $PBKEY $PVKEY $FSDIR/$KNAME/$1 $SADIR/$1
-	mv -f $SADIR/$1 $FSDIR/$KNAME/$1
+	# Sign a target flashable archive with previously set keys.
+	java -jar $SADIR/signapk.jar $PBKEY $PVKEY $FSTMP/$1 $SADIR/$1
+
+	# Move a signed archive back to the source.
+	mv -f $SADIR/$1 $FSTMP/$1
 
 	print "${GRNCL}- $1 was successfully signed!"
 }
 
 __make_kernel()
 {
+	# This function expects configuration to be loaded.
 	(config_loaded) || terminate "3" "Bake configuration"
 
+	# Clean-up the working tree and ensure there is one.
 	prepare_kernel_tree
 
-	[ -f $KNDIR/$KNAME/Makefile ] && cd $KNDIR/$KNAME ||
-	terminate "2" "$KNDIR/$KNAME/Makefile"
+	# Trap into kernel tree as it is guaranteed to exist because of
+	# the previous call.
+	cd $KNDIR/$KNAME
 
-	[ -f $KNDIR/$KNAME/.git ] && BRANC=$(git rev-parse --abbrev-ref HEAD) ||
-	BRANC="unstated"
+	# Try to grab Git branch and export it to bash environment.
+	[ -e $KNDIR/$KNAME/.git ] &&
+	BRANCH=$(git rev-parse --abbrev-ref HEAD) || BRANCH="unstated"
 
-	print "${CYACL}- Building ${KNAME} (\"$BRANC\" branch)..."
+	print "${CYACL}- Building ${KNAME} (\"$BRANCH\" branch)..."
+	# Use generic 3 seconds delay before the build to start.
 	for ((i = 3; i > 0; i--)); do
 		print "${BLUCL}- Build starts in $i"; sleep 0.875
 	done
 
+	# Forcefully tag Git repository if specified.
 	[ $AUTAG == "true" ] && git tag -afm "$TAGNM" $TAGNM &>/dev/null
 
+	# Start the compilation with NRCPS number of threads using a
+	# set-by-config cross compiler. ccache usage should be declared in
+	# device-configuration file.
 	make $DFCFG && make -j$NRCPS $KTYPE REAL_CC="$CACHE ${CROSS_COMPILE}gcc"
 
+	# Ensure the kernel has been compiled succssfully.
 	[ -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE ] &&
-	print "${GRNCL}- ${KNAME} was successfully built!" || terminate "4"
+	print "${GRNCL}- $KNAME was successfully built!" || terminate "4"
 
 	cd $CRDIR
 }
 
-__move_kernel_to_flashable()
+__make_flashable()
 {
+	# This function expects configuration to be loaded.
 	(config_loaded) || terminate "3" "Bake configuration"
-	[ ! -z $1 ] || terminate "1"
 
-	[ -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE ] ||
+	print "${CYACL}- Creating flashable kernel..."
+
+	# Ensure all the required directories and files are actually set up.
+	[ -s $BINST ] || terminate "2" "$BINST"
+	[ -s $FSDIR/$KNAME ] || terminate "2" "$FSDIR/$KNAME"
+	[ -s $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE ] ||
 	terminate "2" "$KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE"
 
-	[ -d $FSDIR/$KNAME/kernel ] || mkdir -p $FSDIR/$KNAME/kernel
+	# Regenerate temporary directory for flashable structure.
+	rm -rf $FSTMP && mkdir $FSTMP
 
-	print "${CYACL}- Moving $KTYPE to $FSDIR/$KNAME/kernel/$1"
-	mv -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE $FSDIR/$KNAME/kernel/$1
-}
+	# Extract both Breakfast installer and device-specific data to the
+	# temporary directory and check whether it all had been made correctly.
+	cp -ax $BINST/* $FSTMP && cp -ax $FSDIR/$KNAME/* $FSTMP/kernel
+	[ -f $FSTMP/kernel/config.sh ] || terminate "2" "$FSTMP/kernel"
 
-make_another_branch()
-{
-	(config_loaded) || terminate "3" "Bake configuration"
-	[ ! -z $1 ] || return 1
+	# Move freshly generated kernel image to the temporary directory.
+	mv -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE $FSTMP/kernel/$KTYPE
 
-	[ -d $KNDIR/$KNAME/.git ] && cd $KNDIR/$KNAME ||
-	terminate "2" "$KNDIR/$KNAME/.git"
-
-	CURBR=$(git rev-parse --abbrev-ref HEAD)
-
-	print "${MGTCL}- Switching to \"$1\" branch..."
-	git checkout $1 &>/dev/null
-	[ "$?" -eq "0" ] || return 1
-
-	print "${MGTCL}- Building \"$1\" branch..."
-
-	__make_kernel
-	__move_kernel_to_flashable "${KTYPE}_o"
-
-	[ -d $KNDIR/$KNAME/.git ] && cd $KNDIR/$KNAME ||
-	terminate "2" "$KNDIR/$KNAME/.git"
-
-	git checkout $CURBR &>/dev/null
-	print "${CYACL}- Going back to \"$CURBR\" branch..."
-}
-
-make_flashable()
-{
-	(config_loaded) || terminate "3" "Bake configuration"
-
-	print "${CYACL}- Creating flashable archive..."
-
+	# Acquire the current date in Year-Month-Day format.
 	DATE=$(date +"%Y%m%d")
-	[ -d $FSDIR/$KNAME/kernel ] || mkdir -p $FSDIR/$KNAME/kernel
-	[ ! -z $1 ] || rm -f $FSDIR/$KNAME/kernel/${KTYPE}_*
 
-	cd $FSDIR/$KNAME && zip -r ${KNAME}-$DATE.zip . && cd $CRDIR
-	[ "$?" -eq "0" ] || terminate "6"
+	# Try to create a zipped archive with all the data collected in
+	# temporary directory. Terminate early in case of a failure.
+	cd $FSTMP && zip -r ${KNAME}-$DATE.zip .
+	[ "$?" -eq "0" ] && cd $CRDIR || terminate "6"
 
+	# Try to sign the newly created flashable archive with the keys created
+	# by OpenSSL software. If the keys are absent, nothing will really
+	# happen, hence this call is safe from all sides.
 	sign_flashable "${KNAME}-$DATE.zip"
 
+	# Clean-up the kernel-ready directory first. Move all the archives
+	# placed within into a special "archived" subdirectory.
 	[ -d $OPDIR/$KNAME/archived ] || mkdir -p $OPDIR/$KNAME/archived
 	mv -f $OPDIR/$KNAME/$KNAME*.zip $OPDIR/$KNAME/archived/ &>/dev/null
 
-	mv $FSDIR/$KNAME/${KNAME}-$DATE.zip $OPDIR/$KNAME/
-	[ -f $OPDIR/$KNAME/${KNAME}-$DATE.zip ] &&
+	# Move the recently created flashable kernel into voided directory,
+	mv $FSTMP/${KNAME}-$DATE.zip $OPDIR/$KNAME/ && rm -rf $FSTMP
+
+	# Ensure all the above has been made successfully
+	[ -s $OPDIR/$KNAME/${KNAME}-$DATE.zip ] &&
 	print "${GRNCL}- ${KNAME}-$DATE.zip was successfully created!" ||
 	terminate "5"
 }
 
 make_kernel()
 {
+	# This function expects configuration to be loaded.
 	(config_loaded) || terminate "3" "Bake configuration"
 
+	# Get current time in Seconds-Nanoseconds format.
 	STIME=$(date +"%s.%N")
 
+	# Compile the kernel and turn it into a flashable archive.
 	__make_kernel
-	__move_kernel_to_flashable $KTYPE
+	__make_flashable
 
-	make_another_branch $1
-	make_flashable $1
-
+	# Clean-up the tree after the process
 	prepare_kernel_tree
 
+	# Get current time in Seconds-Nanosecods format again.
 	ETIME=$(date +"%s.%N")
+	# Now the difference between two times actually represents the elapsed
+	# time for compilation, creating of a flashable and the clean-up.  We
+	# can use bc to calculate the elapsed time properly.
 	RTIME=$(echo "scale=1; ($ETIME - $STIME) / 1" | bc)
 
 	print "${MGTCL}- Kernel was successfully built!"
@@ -258,13 +298,15 @@ make_kernel()
 
 print "${CYACL}- Starting Bake v${BFVER}..."
 
+# Ensure all the critical directories are created and non-empty.
 for DIR in $CFDIR $KNDIR $FSDIR $TCDIR $OPDIR; do
 	[ -s $DIR ] || terminate "0" "$DIR"
 done
 
+# Initialize the script immediately if it was not called in a bash context.
 if [ ! "$0" == "bash" ]; then
 	[ ! -z $1 ] || terminate "1"
 
 	load_config $1
-	make_kernel $2
+	make_kernel
 fi
