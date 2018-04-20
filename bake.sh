f0df1b1d8f1a570a9f8e1dd385257961db901c43
Alex Saiko <solcmdr@gmail.com>
2018-04-20 20:42:44 +0300

    Breakfast: Bake: Introduce v3.3
    After some time I became confused with the names of some internal
    variables because they are not imformative.  A while ago the eyes
    treated this fact not so critical due to a freshment of a new naming,
    but now it is obvious that something must be done with it.
    
    So, I have rewritten all the internal variable names to get rid of this
    5-char limitation as it is extremely uncosy to maintain.  Also I have
    adjusted some pieces of code, including termination function that became
    readable, config checker, which is able to handle void declarations now
    (this is truth for every variable defined check, though).
    
    Signing function has been renamed to sign_package() and is completely
    exportable now, which means that it is finally possible to sign every
    supported package by simply calling sign_package() with that package's
    name as an argument. Much cosier now.
    
    Some miscellaneous stuff was also made, as always.
    
    Change-Id: I33fc4d0ab76ffccac8d6716b816b209683833b0d
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- bake.sh
+++ bake.sh
@@ -12,120 +12,145 @@ clear
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-BFVER=3.2
+# Version number of this script.
+BREAKFAST_VERSION="3.3"
 
+# Flag to reset all applied colors.
+COLOR_RESET=$(tput sgr0)
 # Initial terminal color plan.
-REDCL=$(tput bold)$(tput setaf 1)
-GRNCL=$(tput bold)$(tput setaf 2)
-BLUCL=$(tput bold)$(tput setaf 4)
-MGTCL=$(tput bold)$(tput setaf 5)
-CYACL=$(tput bold)$(tput setaf 6)
-# A flag to reset all applied colors.
-RSTCL=$(tput sgr0)
+COLOR_BOLD=$(tput bold)
+COLOR_GREY=${COLOR_BOLD}$(tput setaf 0)
+COLOR_RED=${COLOR_BOLD}$(tput setaf 1)
+COLOR_GREEN=${COLOR_BOLD}$(tput setaf 2)
+COLOR_YELLOW=${COLOR_BOLD}$(tput setaf 3)
+COLOR_BLUE=${COLOR_BOLD}$(tput setaf 4)
+COLOR_MAGENTA=${COLOR_BOLD}$(tput setaf 5)
+COLOR_CYAN=${COLOR_BOLD}$(tput setaf 6)
 
-# Current directory represents the directory where this script has been called.
+# Source directory represents the directory where this script has been sourced.
 # Root directory represents the directory where this script is located.
-CRDIR=$(pwd)
-RTDIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-CFDIR="$RTDIR/configs"
-KNDIR="$RTDIR/kernels"
-SADIR="$RTDIR/signapk"
-FSDIR="$RTDIR/flashables"
-FSTMP="$FSDIR/.tmp"
-BINST="$FSDIR/breakfast-installer"
-TCDIR="$RTDIR/toolchains"
-OPDIR="$RTDIR/outputs"
+DIR_SOURCE=$(pwd)
+DIR_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+DIR_CONFIGS="$DIR_ROOT/configs"
+DIR_KERNELS="$DIR_ROOT/kernels"
+DIR_SIGNAPK="$DIR_ROOT/signapk"
+DIR_FLASHABLES="$DIR_ROOT/flashables"
+DIR_INSTALLER="$DIR_FLASHABLES/breakfast-installer"
+DIR_TOOLCHAINS="$DIR_ROOT/toolchains"
+DIR_OUTPUTS="$DIR_ROOT/outputs"
 
-# Bake uses the number of threads which is equal to the doubled
-# num of virtual processors to boost the compilation.
-NRCPS=$[ $(grep "processor" /proc/cpuinfo | wc -l) * 2 ]
+# A special directory where a ready installer archive is made up.
+TMP_FLASHABLES="$DIR_FLASHABLES/.tmp"
+
+# Bake uses the number of threads which is equal to the doubled number of all
+# logical processors to boost the compilation.
+NUM_THREADS=$[ $(grep "processor" /proc/cpuinfo | wc -l) * 2 ]
 
 # Automatical Git tagging support.
-AUTAG="true"
-TAGNM="breakfast"
+AUTOTAGGING="true"
+NAMETAG="breakfast"
 
 # A simple color-expecting print function with a 0.125 delay for smooth output
 # of concurrent prints.
-print() { printf "${1}$RSTCL\n"; sleep 0.125; }
+print() { printf "${1}${COLOR_RESET}\n"; sleep 0.125; }
 
 terminate()
 {
 	case "$1" in
-	0) error="Bake is damaged!\n- Broken part --> $2";;
-	1) error="No argument passed!";;
-	2) error="$2 was not found!";;
-	3) error="$2 was not loaded!";;
-	4) error="Kernel stuck in build!";;
-	5) error="Failed to create flashable archive!";;
-	6) error="I/O failure!";;
-	*) error="Unknown error!";;
+	EISDMG)
+		msg="Bake script is damaged!\n! Broken part --> $2";;
+	EINVAL)
+		msg="No argument passed.";;
+	ENOENT)
+		msg="$2 was not found.";;
+	ENLOAD)
+		msg="$2 was not loaded.";;
+	EFAULT)
+		msg="Kernel stuck in build.";;
+	ENFLSH)
+		msg="Unable to create flashable archive.";;
+	EIO)
+		msg="I/O failure.";;
+	*)
+		msg="Unknown error.";;
 	esac
 
 	# Remove temporary flashable directory as it will be overwritten anyway.
-	rm -rf $FSTMP
+	rm -rf $TMP_FLASHABLES
 
-	print "${REDCL}- ERROR: $error\n\nScript terminated with error $1!" &&
+	print "${COLOR_RED}! $msg\n\nScript terminated with errno $1" &&
 	# Print uses a 0.125 seconds delay, hence 2.875 should be used here to
 	# provide overall 3 seconds delay before the exit.
-	sleep 2.875
-	exit $1
-}
-
-prepare_environment()
-{
-	print "${CYACL}- Preparing working environment..."
-	# Unset all the required variables to avoid the conflict.
-	for var in ARCH SUBARCH KNAME KTYPE DFCFG TOOLC; do unset $var; done
+	sleep 2.875 && exit 1
 }
 
 config_loaded()
 {
 	# Check whether all the required variables are actually set.
-	[ ! -z $ARCH ] && [ ! -z $SUBARCH ] && [ ! -z $KNAME ] &&
-	[ ! -z $KTYPE ] && [ ! -z $DFCFG ] && [ ! -z $TOOLC ] &&
-	[ ! -z $CROSS_COMPILE ] || return 1
+	[ ! -z ${ARCH+x} ] && [ ! -z ${SUBARCH+x} ] &&
+	[ ! -z ${KERNEL_SOURCE+x} ] && [ ! -z ${DEFAULT_CONFIGURATION+x} ] &&
+	[ ! -z ${TARGET_TOOLCHAIN+x} ] && [ ! -z ${KERNEL_TYPE+x} ] &&
+	[ ! -z ${CCACHE_USED+x} ] && [ ! -z ${CROSS_COMPILE+x} ] &&
+	return 0 || return 1
+}
 
-	return 0
+prepare_environment()
+{
+	print "${COLOR_CYAN}- Preparing working environment..."
+
+	# Unset all the required variables to avoid the theoretical conflicts.
+	for var in ARCH SUBARCH \
+		   KERNEL_SOURCE \
+		   DEFAULT_CONFIGURATION \
+		   TARGET_TOOLCHAIN \
+		   KERNEL_TYPE \
+		   CCACHE_USED \
+		   CROSS_COMPILE; do
+		unset $var;
+	done
 }
 
 load_config()
 {
 	# This function expects an argument to be passed.
-	[ ! -z $1 ] || terminate "1"
+	[ ! -z ${1+x} ] || terminate EINVAL
 
 	# Try to find a requested configuration in an appropriate directory.
-	CRCFG=$(find $CFDIR/$1* -printf "%f\n" -quit 2>/dev/null)
-	[ ! -z $CRCFG ] || terminate "2" "$1"
+	TARGET_CONFIG=$(find $DIR_CONFIGS/$1* -printf "%f\n" -quit 2>/dev/null)
+	[ ! -z ${TARGET_CONFIG+x} ] || terminate ENOENT "$1"
 
-	print "${MGTCL}- Loading new Bake configuration --> $CRCFG..."
-	# Prepare bash environment before sourcing the configuration file.
+	print "${COLOR_MAGENTA}- Loading Bake configuration --> $TARGET_CONFIG"
+	# Prepare Bash environment before sourcing the new configuration file.
 	prepare_environment
 
-	# Load information from configuration file to bash environment and
+	# Load information from configuration file to Bash environment and
 	# setup cross compiler path.
-	source ${CFDIR}/$CRCFG &&
-	export CROSS_COMPILE="$TCDIR/$TOOLC/bin/${TOOLC}-"
+	source $DIR_CONFIGS/$TARGET_CONFIG &&
+	export CROSS_COMPILE="$DIR_TOOLCHAINS/$TARGET_TOOLCHAIN/bin/${TARGET_TOOLCHAIN}-"
 
 	# Ensure all the things have been made properly.
-	print "${MGTCL}- Validating Bake configuration..."
-	(config_loaded) || terminate "3" "$CRCFG"
+	print "${COLOR_MAGENTA}- Validating Bake configuration..."
+	(config_loaded) || terminate ENLOAD "$TARGET_CONFIG"
 
-	print "${GRNCL}- $CRCFG was successfully loaded!"
+	print "${COLOR_GREEN}- $TARGET_CONFIG was successfully loaded!"
 }
 
 prepare_kernel_tree()
 {
 	# This function cannot work without a configuration loaded.
-	(config_loaded) || terminate "3" "Bake configuration"
+	(config_loaded) || terminate ENLOAD "Bake configuration"
 
 	# Ensure there is a work-ready kernel tree.
-	[ -f $KNDIR/$KNAME/Makefile ] && cd $KNDIR/$KNAME ||
-	terminate "2" "$KNDIR/$KNAME/Makefile"
+	[ -f "$DIR_KERNELS/$KERNEL_SOURCE/Makefile" ] ||
+	terminate ENOENT "$DIR_KERNELS/$KERNEL_SOURCE/Makefile"
 
-	print "${CYACL}- Preparing ${KNAME} tree..."
+	print "${COLOR_CYAN}- Preparing ${KERNEL_SOURCE} tree..."
+
+	# Trap into kernel source tree.
+	cd $DIR_KERNELS/$KERNEL_SOURCE
 
 	# Do basic clean-up of the tree via Makefile-provided methods.
-	[ -f scripts/basic/fixdep ] && (make clean mrproper)
+	[ -e "scripts/basic/fixdep" ] && (make clean mrproper)
 
 	# Remove leftovers of the compilation.
 	rm -rf arch/$ARCH/boot/*.dtb		\
@@ -135,8 +160,7 @@ prepare_kernel_tree()
 	       include/generated
 
 	# Try to remove *unneeded* files via parallel if supported.
-	which parallel &>/dev/null
-	[ "$?" -eq "0" ] &&
+	[ $(which parallel) ] &&
 	find . -type f \( -iname \*.rej			\
 		       -o -iname \*.orig		\
 		       -o -iname \*.bkp			\
@@ -149,131 +173,152 @@ prepare_kernel_tree()
 				| parallel rm -fv { }
 
 	# Tree-specific clean-ups.
-	case "$CRCFG" in
+	case $TARGET_CONFIG in
 	"hammerhead.conf") rm -f arch/$ARCH/mach-msm/smd_rpc_sym.c;;
 	esac
 
-	cd $CRDIR
+	cd $DIR_SOURCE
 }
 
-sign_flashable()
+sign_package()
 {
-	# This function expects both a configuration to be loaded and
-	# an argument to be passed.
-	(config_loaded) || terminate "3" "Bake configuration"
-	[ ! -z $1 ] || terminate "1"
+	# This function expects an argument to be passed.
+	[ ! -z ${1+x} ] || terminate EINVAL
 
 	# Check the presence of the required files.
-	[ -f $SADIR/signapk.jar ] && [ -f $SADIR/keys/*.pk8 ] &&
-	[ -f $SADIR/keys/*.pem ] && [ -f $FSTMP/$1 ] || return 1
+	[ -f $DIR_SIGNAPK/signapk.jar ] && [ -f $DIR_SIGNAPK/keys/*.pk8 ] &&
+	[ -f $DIR_SIGNAPK/keys/*.pem ] && [ -f "$1" ] || return 1
 
-	print "${CYACL}- Signing $1 archive..."
+	local NAME=$(basename "$1")
+	print "${COLOR_CYAN}- Signing $NAME package..."
 
 	# Setup private and public keys.
-	PVKEY=$(find $SADIR/keys/*.pk8 | head -1)
-	PBKEY=$(find $SADIR/keys/*.pem | head -1)
+	PVKEY=$(find $DIR_SIGNAPK/keys/*.pk8 | head -1)
+	PBKEY=$(find $DIR_SIGNAPK/keys/*.pem | head -1)
 
-	# Sign a target flashable archive with previously set keys.
-	java -jar $SADIR/signapk.jar $PBKEY $PVKEY $FSTMP/$1 $SADIR/$1
+	# Sign a target package with the previously set keys.
+	java -jar $DIR_SIGNAPK/signapk.jar $PBKEY $PVKEY $1 $DIR_SIGNAPK/$NAME
 
 	# Move a signed archive back to the source.
-	mv -f $SADIR/$1 $FSTMP/$1
+	mv -f $DIR_SIGNAPK/$NAME $1
 
-	print "${GRNCL}- $1 was successfully signed!"
+	print "${COLOR_GREEN}- $NAME was successfully signed!"
 }
 
 __make_kernel()
 {
 	# This function expects configuration to be loaded.
-	(config_loaded) || terminate "3" "Bake configuration"
+	(config_loaded) || terminate ENLOAD "Bake configuration"
 
 	# Clean-up the working tree and ensure there is one.
 	prepare_kernel_tree
 
-	# Trap into kernel tree as it is guaranteed to exist because of
-	# the previous call.
-	cd $KNDIR/$KNAME
+	# Trap into kernel tree as it is guaranteed to exist because of the
+	# previous call.
+	cd $DIR_KERNELS/$KERNEL_SOURCE
 
-	# Try to grab Git branch and export it to bash environment.
-	[ -e $KNDIR/$KNAME/.git ] &&
-	BRANCH=$(git rev-parse --abbrev-ref HEAD) || BRANCH="unstated"
+	# Try to grab Git branch.
+	[ -e "$DIR_KERNELS/$KERNEL_SOURCE/.git" ] &&
+	BRANCH=$(git rev-parse --abbrev-ref HEAD) || BRANCH="unknown"
 
-	print "${CYACL}- Building ${KNAME} (\"$BRANCH\" branch)..."
-	# Use generic 3 seconds delay before the build to start.
+	print "${COLOR_CYAN}- Building $KERNEL_SOURCE (\"$BRANCH\" branch)..."
+	# Use 3 seconds delay before the build to start.
 	for ((i = 3; i > 0; i--)); do
-		print "${BLUCL}- Build starts in $i"; sleep 0.875
+		print "${COLOR_BLUE}- Build starts in $i..."; sleep 0.875
 	done
 
 	# Forcefully tag Git repository if specified.
-	[ $AUTAG == "true" ] && git tag -afm "$TAGNM" $TAGNM &>/dev/null
+	[ $AUTOTAGGING == "true" ] &&
+	git tag -afm "$NAMETAG" "$NAMETAG" &>/dev/null
 
-	# Start the compilation with NRCPS number of threads using a
+	# Start the compilation with NUM_THREADS number of threads using a
 	# set-by-config cross compiler. ccache usage should be declared in
 	# device-configuration file.
-	make $DFCFG && make -j$NRCPS $KTYPE REAL_CC="$CACHE ${CROSS_COMPILE}gcc"
+	make $DEFAULT_CONFIGURATION &&
+	make -j$NUM_THREADS $KERNEL_TYPE \
+	     REAL_CC="$CCACHE_USED ${CROSS_COMPILE}gcc"
 
 	# Ensure the kernel has been compiled succssfully.
-	[ -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE ] &&
-	print "${GRNCL}- $KNAME was successfully built!" || terminate "4"
+	[ -f "$DIR_KERNELS/$KERNEL_SOURCE/arch/$ARCH/boot/$KERNEL_TYPE" ] ||
+	terminate EFAULT
 
-	cd $CRDIR
+	print "${COLOR_GREEN}- $KERNEL_SOURCE was successfully built!"
+	cd $DIR_SOURCE
 }
 
 __make_flashable()
 {
 	# This function expects configuration to be loaded.
-	(config_loaded) || terminate "3" "Bake configuration"
+	(config_loaded) || terminate ENLOAD "Bake configuration"
 
-	print "${CYACL}- Creating flashable kernel..."
+	print "${COLOR_CYAN}- Creating flashable kernel..."
 
 	# Ensure all the required directories and files are actually set up.
-	[ -s $BINST ] || terminate "2" "$BINST"
-	[ -s $FSDIR/$KNAME ] || terminate "2" "$FSDIR/$KNAME"
-	[ -s $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE ] ||
-	terminate "2" "$KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE"
+	[ -s "$DIR_INSTALLER" ] ||
+	terminate ENOENT "$DIR_INSTALLER"
+
+	[ -s "$DIR_FLASHABLES/$KERNEL_SOURCE" ] ||
+	terminate ENOENT "$DIR_FLASHABLES/$KERNEL_SOURCE"
+
+	[ -s "$DIR_KERNELS/$KERNEL_SOURCE/arch/$ARCH/boot/$KERNEL_TYPE" ] ||
+	terminate ENOENT \
+		 "$DIR_KERNELS/$KERNEL_SOURCE/arch/$ARCH/boot/$KERNEL_TYPE"
 
 	# Regenerate temporary directory for flashable structure.
-	rm -rf $FSTMP && mkdir $FSTMP
+	rm -rf $TMP_FLASHABLES && mkdir $TMP_FLASHABLES
 
 	# Extract both Breakfast installer and device-specific data to the
 	# temporary directory and check whether it all had been made correctly.
-	cp -ax $BINST/* $FSTMP && cp -ax $FSDIR/$KNAME/* $FSTMP/kernel
-	[ -f $FSTMP/kernel/config.sh ] || terminate "2" "$FSTMP/kernel"
+	cp -ax $DIR_INSTALLER/* $TMP_FLASHABLES &&
+	cp -ax $DIR_FLASHABLES/$KERNEL_SOURCE/* $TMP_FLASHABLES/kernel/
+
+	[ -f "$TMP_FLASHABLES/kernel/config.sh" ] ||
+	terminate ENOENT "$TMP_FLASHABLES/kernel"
 
 	# Move freshly generated kernel image to the temporary directory.
-	mv -f $KNDIR/$KNAME/arch/$ARCH/boot/$KTYPE $FSTMP/kernel/$KTYPE
+	mv -f $DIR_KERNELS/$KERNEL_SOURCE/arch/$ARCH/boot/$KERNEL_TYPE \
+	      $TMP_FLASHABLES/kernel/$KERNEL_TYPE
 
 	# Acquire the current date in Year-Month-Day format.
 	DATE=$(date +"%Y%m%d")
 
 	# Try to create a zipped archive with all the data collected in
 	# temporary directory. Terminate early in case of a failure.
-	cd $FSTMP && zip -r ${KNAME}-$DATE.zip .
-	[ "$?" -eq "0" ] && cd $CRDIR || terminate "6"
+	cd $TMP_FLASHABLES && zip -r ${KERNEL_SOURCE}-$DATE.zip .
+	[ "$?" -eq "0" ] && cd $DIR_SOURCE || terminate EIO
 
 	# Try to sign the newly created flashable archive with the keys created
 	# by OpenSSL software. If the keys are absent, nothing will really
 	# happen, hence this call is safe from all sides.
-	sign_flashable "${KNAME}-$DATE.zip"
+	sign_package "$TMP_FLASHABLES/${KERNEL_SOURCE}-$DATE.zip"
+
+	# Ensure "archived" subdirectory actually exists.
+	[ -d "$DIR_OUTPUTS/$KERNEL_SOURCE/archived" ] ||
+	mkdir -p $DIR_OUTPUTS/$KERNEL_SOURCE/archived
 
 	# Clean-up the kernel-ready directory first. Move all the archives
-	# placed within into a special "archived" subdirectory.
-	[ -d $OPDIR/$KNAME/archived ] || mkdir -p $OPDIR/$KNAME/archived
-	mv -f $OPDIR/$KNAME/$KNAME*.zip $OPDIR/$KNAME/archived/ &>/dev/null
+	# placed within it into a "archived" subdirectory mentioned above.
+	mv -f $DIR_OUTPUTS/$KERNEL_SOURCE/$KERNEL_SOURCE*.zip \
+	      $DIR_OUTPUTS/$KERNEL_SOURCE/archived/ &>/dev/null
 
-	# Move the recently created flashable kernel into voided directory,
-	mv $FSTMP/${KNAME}-$DATE.zip $OPDIR/$KNAME/ && rm -rf $FSTMP
+	# Move the recently created flashable kernel into output subdirectory.
+	mv $TMP_FLASHABLES/${KERNEL_SOURCE}-$DATE.zip \
+	   $DIR_OUTPUTS/$KERNEL_SOURCE/
 
-	# Ensure all the above has been made successfully
-	[ -s $OPDIR/$KNAME/${KNAME}-$DATE.zip ] &&
-	print "${GRNCL}- ${KNAME}-$DATE.zip was successfully created!" ||
-	terminate "5"
+	# Remove temporary flashable directory as the work is done.
+	rm -rf $TMP_FLASHABLES
+
+	# Ensure all the above has been made successfully.
+	[ -s "$DIR_OUTPUTS/$KERNEL_SOURCE/${KERNEL_SOURCE}-$DATE.zip" ] ||
+	terminate ENFLSH
+
+	print "${COLOR_GREEN}- ${KERNEL_SOURCE}-$DATE.zip was successfully created!"
 }
 
 make_kernel()
 {
 	# This function expects configuration to be loaded.
-	(config_loaded) || terminate "3" "Bake configuration"
+	(config_loaded) || terminate ENLOAD "Bake configuration"
 
 	# Get current time in Seconds-Nanoseconds format.
 	STIME=$(date +"%s.%N")
@@ -282,7 +327,7 @@ make_kernel()
 	__make_kernel
 	__make_flashable
 
-	# Clean-up the tree after the process
+	# Clean-up the tree after the process.
 	prepare_kernel_tree
 
 	# Get current time in Seconds-Nanosecods format again.
@@ -292,20 +337,24 @@ make_kernel()
 	# can use bc to calculate the elapsed time properly.
 	RTIME=$(echo "scale=1; ($ETIME - $STIME) / 1" | bc)
 
-	print "${MGTCL}- Kernel was successfully built!"
-	print "${CYACL}- Elapsed time: $RTIME seconds"
+	print "${COLOR_MAGENTA}- Kernel was successfully built!"
+	print "${COLOR_CYAN}- Elapsed time: $RTIME seconds"
 }
 
-print "${CYACL}- Starting Bake v${BFVER}..."
+print "${COLOR_CYAN}- Starting Bake v$BREAKFAST_VERSION..."
 
 # Ensure all the critical directories are created and non-empty.
-for DIR in $CFDIR $KNDIR $FSDIR $TCDIR $OPDIR; do
-	[ -s $DIR ] || terminate "0" "$DIR"
+for DIR in $DIR_CONFIGS \
+	   $DIR_KERNELS \
+	   $DIR_FLASHABLES \
+	   $DIR_TOOLCHAINS \
+	   $DIR_OUTPUTS; do
+	[ -s "$DIR" ] || terminate EISDMG "$DIR"
 done
 
-# Initialize the script immediately if it was not called in a bash context.
+# Initialize the script immediately if it was not called in a Bash context.
 if [ ! "$0" == "bash" ]; then
-	[ ! -z $1 ] || terminate "1"
+	[ ! -z ${1+x} ] || terminate EINVAL
 
 	load_config $1
 	make_kernel
